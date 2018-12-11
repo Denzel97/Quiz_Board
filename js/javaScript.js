5b434e779ffee1d59af6ab7b0a9c1a7bc399e72a
Denzel97 <denzelwash97@gmail.com>
2018-12-11 10:27:11 +0300

    js/javaScript.js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,34 +1,45 @@
+var score = 0;
+var total = 5;
+var points = 1;
+var highest = 'total + point';
+
+function init() {
+  sessionStorage.setItem('a1', 'd');
+  sessionStorage.setItem('a2', 'a');
+  sessionStorage.setItem('a3', 'd');
+  sessionStorage.setItem('a4', 'b');
+  sessionStorage.setItem('a5', 'c');
+}
+
 $(document).ready(function() {
   //to hide our questions
   $('.jsForm').hide();
 
-  //to show the questions one by non
+  //to show the questions one by one
   $('#quiz1').show();
 
-  $('#quiz1 #submit').click(function() {
+  $('.jsForm #submit').click(function() {
+    current = $(this).parents('form:first').data('question');
+    next = $(this).parents('form:first').data('question') + 1;
+
     $('.jsForm').hide();
-    $('#quiz2').fadeToggle(1500);
-    return false;
-  });
-  $('#quiz2 #submit').click(function() {
-    $('.jsForm').hide();
-    $('#quiz3').fadeToggle(1500);
-    return false;
-  });
-  $('#quiz3 #submit').click(function() {
-    $('.jsForm').hide();
-    $('#quiz4').fadeToggle(1500);
-    return false;
-  });
-  $('#quiz4 #submit').click(function() {
-    $('.jsForm').hide();
-    $('#quiz5').fadeToggle(1500);
-    return false;
-  });
-  $('#quiz5 #submit').click(function() {
-    $('.jsForm').hide();
-    $('#results').fadeToggle(1500);
+    $('#quiz' + next + '').fadeToggle(1500);
+    process('' + current + '');
     return false;
   });
 
 });
+
+//introduce 'n' to represent Number
+function process(n) {
+  var submitted = $('input[name=quiz' + n + ']:checked').val();
+  if (submitted == sessionStorage.getItem('a' + n + '')) {
+    score++;
+  }
+  if (n == total) {
+    $('#results').html('<h3>Your final score is: ' + score + ' out of 5</h3><a href="index.html">Take Quiz Again</a>');
+  }
+  return false;
+}
+
+window.addEventListener('load', init, false);
